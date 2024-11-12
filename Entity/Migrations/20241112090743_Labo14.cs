aca2639bebf5971a7d87155b11ea37bbafc63f6f
Hubert Vromman <hubert.vromman@gmail.com>
2024-11-12 10:12:14 +0100

    Stored procedure in migration

---
+++ Entity/Migrations/20241112090743_Labo14.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Labo14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE PROCEDURE [dbo].[Login] @Email                     NVARCHAR (320),    @MotDePasse                NVARCHAR (320) AS BEGIN SELECT * FROM utilisateurs WHERE Email = @Email AND MotDePasse = HASHBYTES('SHA2_512', @MotDePasse); END");
+            migrationBuilder.Sql("CREATE PROCEDURE [dbo].[Register] @email NVARCHAR(320), @motDePasse NVARCHAR(320), @nom NVARCHAR(320), @prenom NVARCHAR(320) AS BEGIN INSERT INTO utilisateurs (email, motDePasse, nom, prenom) VALUES (@email, HASHBYTES('SHA2_512', @motDePasse), @nom, @prenom); END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
